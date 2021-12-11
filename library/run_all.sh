09dfb82c189cacb8d1ccf97064ff56e05442698e
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-12-11 09:41:38 +0100

    Fix complete run script

--- library/run_all.sh
+++ library/run_all.sh
@@ -6,12 +6,21 @@ set -e
 # Reset the environments
 rm -rf 01_synthetic_population/environment
 rm -rf 01_synthetic_population/pipeline
+rm -rf 01_synthetic_population/data/*
+rm -rf 01_synthetic_population/output/*
+rm -rf 01_synthetic_population/temp
 
 rm -rf 02_parcel_generation/environment
 rm -rf 02_parcel_generation/code
+rm -rf 02_parcel_generation/input/*
+rm -rf 02_parcel_generation/output/*
+rm -rf 02_parcel_generation/temp
 
 rm -rf 03_vrp/environment
 rm -rf 03_vrp/code
+rm -rf 03_vrp/input/*
+rm -rf 03_vrp/output/*
+rm -rf 03_vrp/temp
 
 # Copy the raw data sets to the population synthesis environment
 cp -r raw_data/* 01_synthetic_population/data
@@ -21,18 +30,25 @@ cp raw_data/osm/rhone-alpes-latest.osm.pbf 03_vrp/input/region.osm.pbf
 cp raw_data/study_area.gpkg 03_vrp/input/area.gpkg
 
 # 1) Population synthesis
-cp raw_data/* 01_synthetic_population/data # Move the raw input data
+cp -r raw_data/* 01_synthetic_population/data # Move the raw input data
 cd 01_synthetic_population
 bash 00_setup.sh
 bash 01_run.sh
 cd ..
 
+rm -rf 01_synthetic_population/environment
+rm -rf 01_synthetic_population/pipeline
+
 # 2) Parcel generation
-cp -r 01_synthetic_population/output/* 02_parcel_generation/input # Copy popualtion input data
+cp 01_synthetic_population/output/* 02_parcel_generation/input # Copy popualtion input data
 cd 02_parcel_generation
 bash 00_setup.sh
 bash 01_run.sh
 cd ..
+
+rm -rf 02_parcel_generation/environment
+rm -rf 02_parcel_generation/code
+
 
 # 3) Solve VRP
 cp 02_parcel_generation/output/* 03_vrp/input # Copy demand input data
